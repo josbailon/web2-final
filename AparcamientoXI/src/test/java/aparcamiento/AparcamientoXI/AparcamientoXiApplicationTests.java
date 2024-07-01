5ba64c08618cdfcf4718c1e3406314292ed2e801
Alexis Jahir Franco Llivipuma <105693703+Nixoby1575@users.noreply.github.com>
2024-06-30 22:16:42 -0500

    Add files via upload

---
+++ AparcamientoXI/src/test/java/aparcamiento/AparcamientoXI/AparcamientoXiApplicationTests.java
@@ -0,0 +1,13 @@
+package aparcamiento.AparcamientoXI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AparcamientoXiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
